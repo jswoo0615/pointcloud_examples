975669cc817e16a93a0dc42bc7bbeb2deb04b669
jswoo0615 <dnwnstkd122@gmail.com>
2023-10-13 15:04:34 +0900

    Extract_indice

---
+++ Extract_indices/build/CMakeFiles/extract_indices.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/siruu/Desktop/SLAM/PointCloud_Tut/Extract_indices/src/extract_indices.cpp" "CMakeFiles/extract_indices.dir/src/extract_indices.cpp.o" "gcc" "CMakeFiles/extract_indices.dir/src/extract_indices.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Extract_indices/build/CMakeFiles/extract_indices.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extract_indices.dir/src/extract_indices.cpp.o"
+  "CMakeFiles/extract_indices.dir/src/extract_indices.cpp.o.d"
+  "extract_indices"
+  "extract_indices.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/extract_indices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
